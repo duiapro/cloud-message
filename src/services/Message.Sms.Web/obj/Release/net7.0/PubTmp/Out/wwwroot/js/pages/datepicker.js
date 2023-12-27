4543faf251dd7228ebb2f3b6cdc9c0a49be33b98
duia <532977252@qq.com>
2023-12-27 16:33:32 +0800

    feat:init

---
+++ src/services/Message.Sms.Web/obj/Release/net7.0/PubTmp/Out/wwwroot/js/pages/datepicker.js
@@ -0,0 +1,5 @@
+//________ Datepicker
+$(".fc-datepicker").datepicker({
+    dateFormat: "yy/mm/dd",
+    monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"]
+});
